c751e76d5ffadfd57502f7e821a855e611597351
Teja <teja.vemulapati@gmail.com>
2021-12-06 18:35:21 +0530

    commit_0612

--- src/basicMethods/PopWindowID.java
+++ src/basicMethods/PopWindowID.java
@@ -2,11 +2,9 @@ package basicMethods;
 
 import java.util.Iterator;
 import java.util.Set;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+
 
 public class PopWindowID {
 
